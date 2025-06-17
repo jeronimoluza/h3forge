483d790bc6a5ca5864caad6469e4de4c0655cd5d
Jerónimo Luza <jero.luza@gmail.com>
2025-06-17 11:17:31 -0300

    h3 working good. should add something to calculate optimal h3 areas

--- scripts/run_pipeline.py
+++ scripts/run_pipeline.py
@@ -4,7 +4,7 @@ region = h3forge.utils.load_wkt(
     "POLYGON((-58.521952 -34.549793, -58.487812 -34.549793, -58.487812 -34.568679, -58.521952 -34.568679, -58.521952 -34.549793))"
 )
 start_date = "2020-01-01"
-end_date = "2020-03-01"
+end_date = "2020-01-20"
 cloud_cover = 0.2
 
 ndvi_array = h3forge.download.sentinel2.get_ndvi(
@@ -17,10 +17,8 @@ ndvi_array = h3forge.download.sentinel2.get_ndvi(
 
 vectors = h3forge.preprocess.vectorize(ndvi_array, region=region)
 
-hexes = h3forge.features.vector_to_h3(vectors, region=region, resolution=10)
+hexes = h3forge.features.vector_to_h3(vectors, region=region, resolution=13)
 
 h3_agg = h3forge.features.h3_aggregation(
-    hexes, region=region, resolution=8, time_agg='daily', strategy='mean'
+    hexes, region=region, resolution=8, time_agg='yearly', strategy='mean'
 )
-
-print(h3_agg)
